72c123e0a97037fe3e65bd2800b3aa667c0f8d67
Sowkarthika991 <73381389+Sowkarthika991@users.noreply.github.com>
2022-12-25 10:57:54 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+     apiKey: "AIzaSyDN8XWSmfbmeE2QpQRXIEsxtuet6FWAxM4",
+  authDomain: "spectagram-6869e.firebaseapp.com",
+  databaseURL: "https://spectagram-6869e-default-rtdb.firebaseio.com",
+  projectId: "spectagram-6869e",
+  storageBucket: "spectagram-6869e.appspot.com",
+  messagingSenderId: "706971053110",
+  appId: "1:706971053110:web:9e7c452215cdf3fedd3c97"
+  };
+  
